d6253d4401373e7dd54d575295b8baec479c2183
Karweiii <karwei2002@gmail.com>
2025-03-03 16:27:47 +0800

    modify next config for s3

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   output:'export',
   distDir: "out",
+  images:{
+    remotePatterns:[
+      {
+        protocol:"https",
+        hostname:'sunwaytechclub.s3.ap-southeast-1.amazonaws.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
